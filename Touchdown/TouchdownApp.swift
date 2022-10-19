13c6f349d5f03456607424849c72c38eb70d46e5
bimo.ez <bimo.ez@kakaomobility.com>
2022-10-19 22:21:49 +0900

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by bimo.ez on 2022/10/19.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
